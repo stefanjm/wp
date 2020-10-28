547fae0ac1f0a67543b7b9cd1fa41a77dda6c9be
stefan joemagi <stefan.joemagi@gmail.com>
2020-10-28 22:10:44 +0200

    Change types to decimal and added a method for calculating volume

--- Core/Models/CupModel.cs
+++ Core/Models/CupModel.cs
@@ -8,7 +8,13 @@ namespace WoodPeckerAngular.Core.Models
     public class CupModel
     {
         public int Id { get; set; }
-        public decimal Diameter { get; set; }
-        public decimal Height { get; set; }
+        public double Diameter { get; set; }
+        public double Height { get; set; }
+
+        public double Volume()
+        {
+            var radius = Diameter / 2;
+            return Math.PI * (radius * radius) * Height;
+        }
     }
 }
